cb5890558dc59384a475796d3e94e29ae2bbf059
omkar_2446 <otambe655@gmail.com>
2026-05-14 12:16:28 +0530

    Configure gunicorn for production

--- backend/app.py
+++ backend/app.py
@@ -11,9 +11,9 @@ CORS(app)
 
 # Email Configuration
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME') # Your email
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD') # Your app password
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
